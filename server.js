476d80ff17d20ec17bedfa2a4394978e1a289eac
Kai <kaiann.fletcher@mail.utoronto.ca>
2020-10-26 02:17:26 -0400

    server.js code for heroku build.

--- server.js
+++ server.js
@@ -19,6 +19,13 @@ app.use(cors());
 //Add routes, only for view since no backed routuing required
 app.use(routes);
 
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
+  
+
 //Start the API server
 app.listen(PORT, function() {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
